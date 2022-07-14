e6b165b2e8e9749b75e3c8b8464279ce87cef2dc
paolalanc <104153171+paolalanc@users.noreply.github.com>
2022-07-14 19:34:36 +0200

    Update main.c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 
 int main ()
 {
- //Percorso di Paola per andare all'universit‡
+ //Percorso di Paola per andare all'universit√†
 
 Alunno Paola = {"Paola", "Lancellotti.", 22, "Nocera Inferiore.","Federico II.", "Ingegneria Aerospaziale"};
 stampa_alunno(Paola);
@@ -67,99 +67,97 @@ macchina paoletta={"Paola","GPL",0.90,11.5,39,3.05,2.10,5.15};
 stampa_macchina(paoletta);
 
 
+ //Percorso Tina per andare all'universita'
+
+Alunno Tina = {"Tina", "Nappo.", 22., "Poggiomarino.","Federico II.", "Ingegneria Aerospaziale"};
+stampa_alunno(Tina);
 
 
-     //Percorso Tina per andare all'universita'
-
-
-    Alunno Tina = {"Tina", "Nappo.", 22., "Poggiomarino.","Federico II.", "Ingegneria Aerospaziale"};
-    stampa_alunno(Tina);
-
-
-    printf("Anche Tina alle ore 9 dovra' sostenere l'esame di Informatica.\n");
-    printf("Per giungere all'universita' usera' la macchina e prendera' la SS268. \n");
-    float tempo_T = (spazio_T / velocita_T)*60;
-    while(velocita_T< limite_di_velocita_min || velocita_T > limite_di_velocita_max)
+printf("Anche Tina alle ore 9 dovra' sostenere l'esame di Informatica.\n");
+printf("Per giungere all'universita' usera' la macchina e prendera' la SS268. \n");
+float tempo_T = (spazio_T / velocita_T)*60;
+while(velocita_T< limite_di_velocita_min || velocita_T > limite_di_velocita_max)
 
 {
-    printf("\nInserire la velocita' in SS268: ",velocita_T);
-    scanf("%f", &velocita_T);
+  printf("\nInserire la velocita' in SS268: ",velocita_T);
+  scanf("%f", &velocita_T);
 
-
-    if (velocita_T >=limite_di_velocita_min &&velocita_T<= limite_di_velocita_max )
+if (velocita_T >=limite_di_velocita_min &&velocita_T<= limite_di_velocita_max )
 
 {
-    tempo_T= (spazio_T / velocita_T)*60;
-    printf( "\n\n A questa velocita' impieghera': %5.2f! minuti .\n", tempo_T);
-    printf("Arrivera' in tempo per l'esame? \n");
+  tempo_T= (spazio_T / velocita_T)*60;
+  printf( "\n\n A questa velocita' impieghera': %5.2f! minuti .\n", tempo_T);
+  printf("Arrivera' in tempo per l'esame? \n");
 }
 
-    else
+ else
+  
 {
-    printf ("Non e' possibile viaggiare a questa velocita'.\n");
-    printf("Puoi viaggiare in SS268 con una velocita minima di 50 km/h e con una velocita massima di 90 km/h.");
+  printf ("Non e' possibile viaggiare a questa velocita'.\n");
+  printf("Puoi viaggiare in SS268 con una velocita minima di 50 km/h e con una velocita massima di 90 km/h.");
 }
 }
 
-    do
+do
+ 
 {
+   printf("\nInserire orario di partenza:   ",h_partenza, m_partenza);
+   scanf("%d", &h_partenza);
+   scanf("%d", &m_partenza);
+   m_arrivo=m_partenza+tempo_T;
 
-    printf("\nInserire orario di partenza:   ",h_partenza, m_partenza);
-    scanf("%d", &h_partenza);
-    scanf("%d", &m_partenza);
-    m_arrivo=m_partenza+tempo_T;
-
-
-    if(m_arrivo<=59 && h_partenza<=8)
+if(m_arrivo<=59 && h_partenza<=8)
 
 {
-    printf("Arrivera' alle %d.%d.\n",h_partenza, m_arrivo);
-    printf("Tina arrivera' in tempo per sostenere l'esame.\n\n");
+   printf("Arrivera' alle %d.%d.\n",h_partenza, m_arrivo);
+   printf("Tina arrivera' in tempo per sostenere l'esame.\n\n");
 }
 
-    else if (m_arrivo>59 && h_partenza==7)
+else if (m_arrivo>59 && h_partenza==7)
 
 {
-    h_arrivo= h_partenza+1;
-    int m3_arrivo= -(60-m_arrivo);
-    printf("Arrivera' alle %d.%d\n",h_arrivo,m3_arrivo);
-    printf("Tina arrivera' in tempo per sostenere l'esame.\n\n");
+   h_arrivo= h_partenza+1;
+   int m3_arrivo= -(60-m_arrivo);
+   printf("Arrivera' alle %d.%d\n",h_arrivo,m3_arrivo);
+   printf("Tina arrivera' in tempo per sostenere l'esame.\n\n");
 }
 
-    else if (m_arrivo>59 && h_partenza==8)
+ else if (m_arrivo>59 && h_partenza==8)
 
 {
-    h_arrivo=h_partenza+1;
-    int m2_arrivo= -(60- m_arrivo);
-    printf ("Tina arrivera' alle %d.%d\n", h_arrivo,m2_arrivo);
-    printf("Tina non arrivera' in tempo per sostenere l'esame.\nRiprova!");
+  h_arrivo=h_partenza+1;
+  int m2_arrivo= -(60- m_arrivo);
+  printf ("Tina arrivera' alle %d.%d\n", h_arrivo,m2_arrivo);
+  printf("Tina non arrivera' in tempo per sostenere l'esame.\nRiprova!");
 
 }
 
 }
-   while(m_arrivo>59 && h_arrivo==9);
-   macchina concetta={"Tina","benzina",2.00,13.5,32,4.74,0,4.74};
-   stampa_macchina(concetta);
+ while(m_arrivo>59 && h_arrivo==9);
+ 
+ macchina concetta={"Tina","benzina",2.00,13.5,32,4.74,0,4.74};
+ stampa_macchina(concetta);
 
-   printf("\t\t\t\t\tQUIZ!\n");
+   
+ printf("\t\t\t\t\tQUIZ!\n");
 
-    int alunno;
-
-    printf("Se Paola e Tina partono allo stesso orario e viaggiano alla massima velocita' consentita sui rispettivi percorsi:\n");
-    printf("Chi arriva prima all' Universita'? \n");
-    printf("Scegli il numero:\n");
-    printf("1)PAOLA\n2)TINA\n");
-    scanf("%d", &alunno);
-    switch(alunno)
+ int alunno;
+ 
+ printf("Se Paola e Tina partono allo stesso orario e viaggiano alla massima velocita' consentita sui rispettivi percorsi:\n");
+ printf("Chi arriva prima all' Universita'? \n");
+ printf("Scegli il numero:\n");
+ printf("1)PAOLA\n2)TINA\n");
+ scanf("%d", &alunno);
+ switch(alunno)
 
 {
-    case PAOLA :
-    printf("HAI INDOVINATO!\n\n\n ");
-    break;
+  case PAOLA :
+  printf("HAI INDOVINATO!\n\n\n ");
+  break;
 
-    case TINA :
-    printf("HAI SBAGLIATO!\a\nTina impieghera' 3 minuti in piu'.\n\n\n");
-    break;
+  case TINA :
+  printf("HAI SBAGLIATO!\a\nTina impieghera' 3 minuti in piu'.\n\n\n");
+  break;
 
 }
 
